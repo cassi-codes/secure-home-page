1bf35b3aaf42528d5f53289646e904874e64ed33
cassi-codes <ihsoy.aaa.unonu@gmail.com>
2025-12-24 02:35:54 +0900

    loginValidateの修正

--- public/javascripts/admin/formValidate.js
+++ public/javascripts/admin/formValidate.js
@@ -1,6 +1,22 @@
 (() => {
   "use strict";
   const form = document.querySelector(".needs-validation");
+  const nameInput = document.getElementById("name");
+  const passwordInput = document.getElementById("password");
+
+  nameInput.addEventListener("blur", function () {
+    if (this.value.length >= 1) {
+      this.classList.add("is-valid");
+      this.classList.remove("is-invalid");
+    }
+  });
+
+  passwordInput.addEventListener("blur", function () {
+    if (this.value.length >= 8) {
+      this.classList.add("is-valid");
+      this.classList.remove("is-invalid");
+    }
+  });
 
   form.addEventListener(
     "submit",
